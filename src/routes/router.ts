b623d30c67dee16e49d84df016e5ffd22b039caf
p3isman <pedro_eisman@outlook.com>
2022-06-24 19:06:47 +0200

    Rename router to routers to fix error (step 1)

--- src/routes/router.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const router = express.Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.send('Server up and running');
-});
-
-export default router;
